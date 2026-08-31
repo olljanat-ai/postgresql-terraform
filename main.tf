f58ac79f4d561391bf8f0cbe5c9669475671bb6f
Olli Janatuinen <olli.janatuinen@gmail.com>
2026-08-31 15:12:25 +0000

    Correct Key Vault delegation

--- main.tf
+++ main.tf
@@ -298,6 +298,7 @@ resource "azurerm_role_assignment" "key_vault_secrets_officer" {
   scope                = azurerm_key_vault.this[0].id
   role_definition_name = "Key Vault Secrets Officer"
   principal_id         = data.azurerm_client_config.current.object_id
+  principal_type       = "User"
 
   # The role is assigned to the identity Terraform runs as, which exists by
   # definition, so the provider does not have to wait for Entra to replicate it.
